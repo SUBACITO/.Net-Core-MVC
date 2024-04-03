e592ecac7bb057c4d85c23f9806c15bca8ab0eb4
SUBACITO <subacitodev@gmail.com>
2024-04-03 08:36:17 +0700

    Create New project

---
+++ myDotNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myDotNetCoreApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
