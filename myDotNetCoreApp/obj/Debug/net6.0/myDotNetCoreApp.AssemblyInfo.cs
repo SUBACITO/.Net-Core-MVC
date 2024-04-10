173eec7667566e525cb730dfc8b1803ce802b0f7
SUBACITO <subacitodev@gmail.com>
2024-04-10 08:02:29 +0700

    Today i feel not good

--- myDotNetCoreApp/obj/Debug/net6.0/myDotNetCoreApp.AssemblyInfo.cs
+++ myDotNetCoreApp/obj/Debug/net6.0/myDotNetCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myDotNetCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79848f226e800a58587b8f7f85f76618465fcf09")]
 [assembly: System.Reflection.AssemblyProductAttribute("myDotNetCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myDotNetCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
