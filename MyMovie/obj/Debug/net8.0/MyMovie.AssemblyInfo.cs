7f986ef00f3ec04548c609b92843e8cbf6904178
SUBACITO <subacitodev@gmail.com>
2024-04-17 07:54:30 +0700

    ...

--- MyMovie/obj/Debug/net8.0/MyMovie.AssemblyInfo.cs
+++ MyMovie/obj/Debug/net8.0/MyMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ffefe7a760087f2d1bd0f3eb1903b92e68313d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f865a8e5d4589b5270fde3489d643e16ac623740")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
