7b05648b2bf84444213d85fcaa0fd1df2caa81f4
SUBACITO <subacitodev@gmail.com>
2024-04-17 09:29:22 +0700

    new

--- MyMovie/Controllers/MoviesController.cs
+++ MyMovie/Controllers/MoviesController.cs
@@ -20,9 +20,37 @@ namespace MyMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
@@ -47,6 +75,12 @@ namespace MyMovie.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // POST: Movies/Create
@@ -153,5 +187,7 @@ namespace MyMovie.Controllers
         {
             return _context.Movie.Any(e => e.Id == id);
         }
+
+
     }
 }
