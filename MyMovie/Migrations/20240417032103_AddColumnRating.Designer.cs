51db4f0a78b010d1171f59482f746e076f16bd6d
SUBACITO <subacitodev@gmail.com>
2024-04-17 10:34:14 +0700

    New

--- MyMovie/Migrations/20240417032103_AddColumnRating.Designer.cs
+++ MyMovie/Migrations/20240417032103_AddColumnRating.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
@@ -10,9 +11,11 @@ using MvcMovie.Data;
 namespace MyMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20240417032103_AddColumnRating")]
+    partial class AddColumnRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
--- MyMovie/obj/Debug/net8.0/MyMovie.AssemblyInfo.cs
+++ MyMovie/obj/Debug/net8.0/MyMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d0e032084599babfe9d89b1b2c44bb3f5ba4b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b05648b2bf84444213d85fcaa0fd1df2caa81f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
